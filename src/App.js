0b49c888afc711c95a1e5b3c2ceaf40dc0ee0611
vakash12 <99234445+vakash12@users.noreply.github.com>
2022-04-21 17:18:15 +0530

    add paragraph

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
               <div className="col-md-6">
                   <h1 className="text-center" style={style}>React User Application</h1>
                   <h2 className="text-center" style={style}>Hello vakash test via git gub actions</h2>
+                  <p className="text-center">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. </p>
                   <Switch>
                       <Route path="/" exact component={ListUserComponent} />
                       <Route path="/users" component={ListUserComponent} />
